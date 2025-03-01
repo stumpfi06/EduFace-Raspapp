3410472b2cfb2fe4353398b494731dc983ab3fdd
stumpfi00 <maxiasd@icloud.com>
2025-03-01 13:02:10 +0100

    altered env variable names

--- eduface-raspapp-backend/util/firebase.config.ts
+++ eduface-raspapp-backend/util/firebase.config.ts
@@ -3,20 +3,18 @@ import { getFirestore } from "firebase/firestore"
 import { config } from "dotenv";
 config();
 
-
 const firebaseConfig = {
-  apiKey: process.env.APIKEY ,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-
-export {db};
+export { db };
